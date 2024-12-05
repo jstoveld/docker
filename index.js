6319f090200d88cc065bb375b3a298a63295f2de
John Stoveld <jstoveld@gmail.com>
2024-12-05 18:59:45 -0500

    Authentication works now

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require('express')
-const app = express()
+const basicAuth = require('basic-auth');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
 const port = 3000;
+
+const authenticate = (req, res, next) => {
+    const user = basicAuth(req);
+    if (user && user.name == process.env.USERNAME && user.pass == process.env.PASSWORD) {
+        return next();
+    } else {
+        res.set('WWW-Authenticate', 'Basic ream="example"');
+        return res.status(401).send('Auth Required.');
+    }
+};
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.get('/secret', (req, res) => {
+app.get('/secret', authenticate, (req, res) => {
     res.send('This secret is super secret.');
 });
 
