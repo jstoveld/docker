8fd1ae2ec04edb6062c3b697118ef6ee601bcbbe
John Stoveld <jstoveld@gmail.com>
2024-12-06 11:36:07 -0500

    Docker service and authentication middleware works.

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-const express = require('express')
+const express = require('express');
 const basicAuth = require('basic-auth');
 const dotenv = require('dotenv');
 
 dotenv.config();
+
+const USERNAME = process.env.USERNAME;
+const PASSWORD = process.env.PASSWORD;
 
 const app = express();
 const port = 3000;
 
 const authenticate = (req, res, next) => {
     const user = basicAuth(req);
-    if (user && user.name == process.env.USERNAME && user.pass == process.env.PASSWORD) {
+    console.log('Received User:', user);
+    if (user && user.name === process.env.USERNAME && user.pass === process.env.PASSWORD) {
+        console.log('Authentication successful');
         return next();
     } else {
-        res.set('WWW-Authenticate', 'Basic ream="example"');
+        console.log('Authentication failed');
+        res.set('WWW-Authenticate', 'Basic realm="example"');
         return res.status(401).send('Auth Required.');
     }
 };
-
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
